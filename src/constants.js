c757f3361aeb07225593dbc6faf12bb59d4cf095
Jian Han <jianhan@mail.com>
2018-02-22 21:04:09 +1000

    [COURSE] Finished the form for upsert course

---
+++ src/constants.js
@@ -0,0 +1,1 @@
+export const dateTimePickerFormat = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: '2-digit' }
--- src/main.ts
+++ src/main.ts
@@ -13,11 +13,19 @@ import VeeValidate from 'vee-validate'
 import store from './store'
 import VuexFlash from 'vuex-flash'
 import { sync } from 'vuex-router-sync'
+import VCalendar from 'v-calendar';
+import 'v-calendar/lib/v-calendar.min.css'
+import Datetime from 'vue-datetime'
+// You need a specific loader for CSS files
+import 'vue-datetime/dist/vue-datetime.css'
 
 sync(store, router)
 Vue.use(VuexFlash, { mixin: true })
 Vue.use(BootstrapVue)
 Vue.use(VeeValidate)
+// Use v-calendar, v-date-picker & v-popover components
+Vue.use(VCalendar);
+Vue.use(Datetime)
 Object.defineProperty(Vue.prototype, '$_', { value: _ })
 Object.defineProperty(Vue.prototype, '$moment', { value: moment })
 Object.defineProperty(Vue.prototype, '$env', { value: env })
