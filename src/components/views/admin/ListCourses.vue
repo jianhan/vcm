0bef591165f233e8f96cf8e923c00e6ef4272e5d
Jian Han <jianhan@mail.com>
2018-02-27 23:13:21 +1000

    [DEV] Added pagination to courses

--- src/components/views/admin/ListCourses.vue
+++ src/components/views/admin/ListCourses.vue
@@ -1,23 +1,31 @@
 <template>
   <div>
     <flash-message variant="success"></flash-message>
+    asdf
     <vuetable ref="vuetable"
               api-url="http://courses-management.localhost/api/v1/courses"
               :fields="fields"
               :http-options="authHeader"
+              pagination-path=""
+              @vuetable:pagination-data="onPaginationData"
     ></vuetable>
+    <vuetable-pagination ref="pagination"
+                         @vuetable-pagination:change-page="onChangePage"
+    ></vuetable-pagination>
   </div>
 </template>
 
 <script>
 import Vuetable from 'vuetable-2/src/components/Vuetable'
+import VuetablePagination from 'vuetable-2/src/components/VuetablePagination'
 import authMixin from '@/mixins/auth'
 
 export default {
   name: 'admin-list-courses',
   mixins: [authMixin],
   components: {
-    Vuetable
+    Vuetable,
+    VuetablePagination
   },
   data () {
     return {
@@ -38,8 +46,13 @@ export default {
   methods: {
     formatBoolean (val) {
       return val
+    },
+    onPaginationData (paginationData) {
+      this.$refs.pagination.setPaginationData(paginationData)
+    },
+    onChangePage (page) {
+      this.$refs.vuetable.changePage(page)
     }
-
   }
 }
 </script>
